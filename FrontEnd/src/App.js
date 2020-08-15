d99c1e5d9815cff79db2259722833d15a4493abc
[Yashwin Sudarshan s3781718] <s3781718@student.rmit.edu.au>
2020-08-16 01:37:17 +1000

    Created login page (does not have login functionality); Also added functionalityto return to the home page.

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import NavigationBar from "./components/Navbar.js"
-import Body from "./components/Body.js"
-import Main from "./components/Main.js"
-import Footer from './components/Footer.js';
+import NavigationBar from "./components/layouts/Navbar.js";
+import Footer from './components/layouts/Footer.js';
+import {Switch, Route} from 'react-router-dom';
+import LoginPage from './components/pages/LoginPage';  
+import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
     <div>
       
       <NavigationBar />
-      <Body />
-      <Main />
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
       <br />
       <Footer />
 
