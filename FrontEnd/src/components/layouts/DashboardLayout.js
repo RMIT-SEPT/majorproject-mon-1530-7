a1cff89a86a027d4e416dafe325562f9a3479319
“Amy <s3783694@student.rmit.edu.au>
2020-08-23 17:38:57 +1000

    Add table template

--- FrontEnd/src/components/layouts/DashboardLayout.js
+++ FrontEnd/src/components/layouts/DashboardLayout.js
@@ -12,7 +12,33 @@ function DashboardLayout() {
         <Row>
           <Col className="shadow p-3 mb-5 bg-white rounded" id="col-custom" xs={6} md={{ span: 6, offset: 0 }}>
             <h5 className="h5-main">Current Bookings</h5>
-            <p>Current Bookings Go here</p>
+            <table class="table table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col"></th>
+                  <th scope="col">Service</th>
+                  <th scope="col">Employee</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Service 1</td>
+                  <td>Mia Smith</td>
+                  <td>12/09/2020</td>
+                  <td>10:30am</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Service 2</td>
+                  <td>Kai Lomia</td>
+                  <td>14/09/2020</td>
+                  <td>12:30pm</td>
+                </tr>
+              </tbody>
+            </table>
           </Col>
           <Col className="shadow p-3 mb-5 bg-white rounded" xs={6} md={{ span: 5, offset: 1 }}>
             <h5 className="h5-main">Upcoming Bookings</h5>
