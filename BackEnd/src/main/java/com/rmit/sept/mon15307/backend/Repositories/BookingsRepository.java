ce9973a67dea48b6c1ce4c1e42903c86335bfea7
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-08-26 14:27:32 +1000

    Began Development of Booking Backend Adapted Code from tutorial, this code has a basic model of a booking and a function to save it to a booking repository

---
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/Repositories/BookingsRepository.java
@@ -0,0 +1,12 @@
+package com.rmit.sept.mon15307.backend.Repositories;
+
+import com.rmit.sept.mon15307.backend.model.Booking;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingsRepository extends CrudRepository<Booking, Long> {
+
+        @Override
+        Iterable<Booking> findAll();
+}
---
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/model/Booking.java
@@ -0,0 +1,53 @@
+package com.rmit.sept.mon15307.backend.model;
+
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.*;
+import java.util.Date;
+
+
+
+@Entity
+public class Booking {
+    
+   
+    private String customer_id;
+    private String staff_id;
+    private int booked_time;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date booked_date;
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date created_At;
+    
+
+    public Booking() {
+    }
+
+    public String getCustomer() {
+        return customer_id;
+    }
+    public void setCustomer(String customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getStaff() {
+        return staff_id;
+    }
+    public void setStaff(String staff_id) {
+        this.staff_id = staff_id;
+    } 
+
+    public Date getBookedDate() {
+        return booked_date;
+    }
+    public void setBookedDate(Date booked_date) {
+        this.booked_date = booked_date;
+    }
+
+    public Date getCreatedDate() {
+        return created_At;
+    }
+    public void setCreatedDate(Date created_At) {
+        this.created_At = created_At;
+    }
+
+}
