36486f9ff5ebc4d0de705d38c197cda795557e05
[Yashwin Sudarshan s3781718] <s3781718@student.rmit.edu.au>
2020-08-30 01:13:38 +1000

    Changed colour of datepicker to enhance aesthetics; Created error modal message for an invalid booking (currently only functions when the make booking button is clicked)

---
+++ FrontEnd/src/components/modals/CustomerBookingPageErrorModal.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+function CustomerBookingPageErrorModal(props) {
+    return (
+        
+        <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Error
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Invalid Booking</h4>
+          <p>
+            Please select one service, one employee, and an available date and time, 
+            then click "Make Booking"
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+    );
+}
+
+export default CustomerBookingPageErrorModal
--- FrontEnd/src/components/pages/CustomerBookingPage.js
+++ FrontEnd/src/components/pages/CustomerBookingPage.js
@@ -6,12 +6,14 @@ import {Jumbotron, Button, ToggleButtonGroup, ToggleButton, Row, Col, Form} from
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import '../../react_dates_overrides.css';
-import {DateRangePicker, SingleDatePicker, DayPickerRangeController} from 'react-dates';
+// import '../../react_dates_overrides.css';
+// import {DateRangePicker, SingleDatePicker, DayPickerRangeController} from 'react-dates';
 
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import "../../react-datepicker_overrides.css";
 import { setMinutes, setHours } from 'date-fns'
+import CustomerBookingPageErrorModal from '../modals/CustomerBookingPageErrorModal'
 
 
 
@@ -19,10 +21,11 @@ const DateSelector = () => {
     const[startDate, setStartDate] = useState(setHours(setMinutes(new Date(), 0), 10)
     );
     return (
-        <DatePicker selected={startDate}
+        <DatePicker className="datePickerContainer" selected={startDate}
                     onChange={date => setStartDate(date)}
                     showTimeSelect
                     includeTimes={[
+                        setHours(setMinutes(new Date(), 0), 10),
                         setHours(setMinutes(new Date(), 30), 10),
                         setHours(setMinutes(new Date(), 0), 11),
                         setHours(setMinutes(new Date(), 30), 11),
@@ -40,69 +43,15 @@ const DateSelector = () => {
     );
 }
 
+// const [modalShow, setModalShow] = React.useState(false);
 
+function CustomerBookingPage() {
 
-// const Example = () => {
-//     const [startDate, setStartDate] = useState(new Date());
-//     const ExampleCustomInput = ({ value, onClick }) => (
-//         <button className="example-custom-input" onClick={onClick}>
-//           {value}
-//         </button>
-//     );
-//     return (
-//       <DatePicker selected={startDate} 
-//                 onChange={date => setStartDate(date)}
-//                 customInput={<ExampleCustomInput />}
-//         />
-//     );
-//   };
+    const [modalShow, setModalShow] = React.useState(false);
 
+    const [radio, setRadio] = React.useState(1);
 
-// import DayPickerInput from 'react-day-picker/DayPickerInput';
-// import { DateUtils } from 'react-day-picker';
-// import 'react-day-picker/lib/style.css';
-// import '../../react_day_picker_input_overrides.css';
-
-// import dateFnsFormat from 'date-fns/format';
-// import dateFnsParse from 'date-fns/parse';
-
-// function parseDate(str, format, locale) {
-//   const parsed = dateFnsParse(str, format, new Date(), { locale });
-//   if (DateUtils.isDate(parsed)) {
-//     return parsed;
-//   }
-//   return undefined;
-// }
-
-// function formatDate(date, format, locale) {
-//   return dateFnsFormat(date, format, { locale });
-// }
-
-
-// const FORMAT = 'dd/MM/yyyy';
-
-
-
-
-
-
-
-
-class CustomerBookingPage extends Component {
-
-
-    // constructor(props){
-    //     super(props);
-    //     this.state ={
-    //         date: null,
-    //         block: true,
-    //         small: true,
-    //         horizontalMargin: 5,
-    //         noBorder: true
-    //     }
-    // }
-
-    render() {
+    // render() {
         return (
             <Jumbotron id="jumbotron-cus-book-page">
                 <Container>
@@ -113,7 +62,10 @@ class CustomerBookingPage extends Component {
                         <Card className="shadow p-3 mb-5 bg-white rounded"  border="light" style={{ width: '68rem' }}>
                             <Card.Body>
                             <Card.Title>Available Services</Card.Title><br/>
-                            <ToggleButtonGroup type="radio" name="options" className="mb-2" vertical>
+                            <ToggleButtonGroup type="radio" name="options" 
+                                // onChange={(e) => {setRadio(e.target.value)}}
+                                className="mb-2" 
+                                vertical>
                                 <ToggleButton variant="outline-primary" value={1} size="lg">Service 1</ToggleButton><br/>
                                 <ToggleButton variant="outline-primary" value={2} size="lg">Service 2</ToggleButton><br/>
                                 <ToggleButton variant="outline-primary" value={3} size="lg">Service 3</ToggleButton>
@@ -123,79 +75,42 @@ class CustomerBookingPage extends Component {
                         <Card className="shadow p-3 mb-5 bg-white rounded" border="light" style={{ width: '68rem' }}>
                             <Card.Body>
                             <Card.Title>Available Employees</Card.Title><br/>
-                            <ToggleButtonGroup type="radio" name="options" className="mb-2" vertical>
-                                <ToggleButton variant="outline-primary" value={1} size="lg">Mia</ToggleButton><br/>
-                                <ToggleButton variant="outline-primary" value={2} size="lg">Athena</ToggleButton><br/>
-                                <ToggleButton variant="outline-primary" value={3} size="lg">Kai</ToggleButton>
+                            <ToggleButtonGroup type="radio" name="options" 
+                                // onChange={(e) => {setRadio(e.target.value)}}
+                                className="mb-2" 
+                                vertical>
+                                <ToggleButton variant="outline-primary" value={4} size="lg">Mia</ToggleButton><br/>
+                                <ToggleButton variant="outline-primary" value={5} size="lg">Athena</ToggleButton><br/>
+                                <ToggleButton variant="outline-primary" value={6} size="lg">Kai</ToggleButton>
                             </ToggleButtonGroup>
                             </Card.Body>
                         </Card>
                         <Card className="shadow p-3 mb-5 bg-white rounded" border="light" style={{ width: '68rem' }}>
                             <Card.Body>
                             <Card.Title>Select Time</Card.Title><br/>
-                            
-
                             <DateSelector/>
-
-
-                            
                             </Card.Body>
                         </Card>
                     </CardDeck>
                 </Container>
                 <Container className="makeCusBooking">
-                    <Button variant="primary" size="lg" type="submit">Make Booking</Button>
+                    <Button variant="primary" size="lg" type="submit"
+                        onClick={() => setModalShow(true)}>Make Booking</Button>
+                    <CustomerBookingPageErrorModal 
+                        className="customer-booking-page-error-modal"
+                        show={modalShow}
+                        onHide={() => setModalShow(false)}
+                        />
+                            
                 </Container>
           </Jumbotron>
         )
     }
-}
+// }
 
 export default CustomerBookingPage;
 
 
-// <SingleDatePicker
-//                                 date={this.state.date} 
-//                                 onDateChange={date => this.setState({ date })} 
-//                                 focused={this.state.focused}
-//                                 onFocusChange={({ focused }) => this.setState({ focused })} 
-//                                 id="booking_date_selection" 
-//                                 block={this.state.block}
-//                                 // small={this.state.small}
-//                                 horizontalMargin={this.state.horizontalMargin}
-//                                 // noBorder={this.state.noBorder}
-//                             /><br/>
 
 
-// <Form>
-//                                 <Form.Group controlId="Date">
-//                                     <Form.Control type="date" name="Date" required placeholder="Date"/>
-//                                 </Form.Group>
-//                             </Form>
 
-
-// <DayPickerInput
-//                             formatDate={formatDate}
-//                             format={FORMAT}
-//                             parseDate={parseDate}
-//                             placeholder={`${dateFnsFormat(new Date(), FORMAT)}`}
-//                           />
-
-
-// <ToggleButtonGroup type="radio" name="options" vertical><br/>
-                                
-//                                 <ToggleButton variant="outline-primary" value={1} size="lg">10:00 am</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={3} size="lg">11:00 am</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={5} size="lg">12:00 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={7} size="lg">1:00 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={9} size="lg">2:00 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={11} size="lg">3:00 pm</ToggleButton><br/>
-                            
-                            
-//                                 <ToggleButton variant="outline-primary" value={2} size="lg">10:30 am</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={4} size="lg">11:30 am</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={6} size="lg">12:30 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={8} size="lg">1:30 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={10} size="lg">2:30 pm</ToggleButton><br/>
-//                                 <ToggleButton variant="outline-primary" value={12} size="lg">3:30 pm</ToggleButton>
-//                             </ToggleButtonGroup>
