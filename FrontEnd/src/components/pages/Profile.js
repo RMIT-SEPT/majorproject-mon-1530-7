c00224cb21ec91225ef3db157fe10bc29dfe976b
“Amy <s3783694@student.rmit.edu.au>
2020-09-04 15:14:14 +1000

    Add edit profile button

--- FrontEnd/src/components/pages/Profile.js
+++ FrontEnd/src/components/pages/Profile.js
@@ -9,12 +9,14 @@ class Profile extends Component {
                 <Col className="shadow p-3 mb-5 bg-white rounded" id="profile-col" xs={6} md={{ span: 6, offset: 0 }}>
                     <h5 className="h5-main">Name</h5>
                     <p>Mia</p>
+                    <h5 className="h5-main">Email/Username</h5>
+                    <p>mia@email.com</p>
                     <h5 className="h5-main">Address</h5>
                     <p>123 Sample St</p>
                     <h5 className="h5-main">Phone Number</h5>
                     <p>0123456789</p>
-                    <h5 className="h5-main">Email/Username</h5>
-                    <p>mia@email.com</p>
+                    
+                    <button className="edit-btn">Edit Profile</button>
                 </Col>
             </div>
         )
