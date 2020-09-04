b68b09fd26392bbedd13ccaa79a8981fe5face64
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-09-04 15:32:49 +1000

    Added User model and matched booking model with the backend model in notion

--- BackEnd/src/main/java/com/rmit/sept/mon15307/backend/Repositories/BookingsRepository.java
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/Repositories/BookingsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookingsRepository extends CrudRepository<Booking, Long> {
 
-        Booking findByBookingId(String bookingId);
+        Booking findByBookingId(String BookingId);
         @Override
         Iterable<Booking> findAll();
 }
