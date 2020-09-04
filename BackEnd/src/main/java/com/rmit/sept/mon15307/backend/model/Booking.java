d8122a5684f1e614f09ffda9d06284885a1e07a0
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-09-04 15:32:49 +1000

    Added User model and matched booking model with the backend model in notion

--- BackEnd/src/main/java/com/rmit/sept/mon15307/backend/model/Booking.java
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/model/Booking.java
@@ -13,44 +13,57 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
   //@NotBlank(message = "Customer ID is required")
     private String bookingId;
+    private String status;
     private String customer_id;
-    private String staff_id;
-    private int booked_time;
-    @JsonFormat(pattern = "yyyy-mm-dd")
-    private Date booked_date;
+    private String employee_id;
+    private String product_id;
+    private String schedule_id;
+    @JsonFormat(pattern = "hh-mm")
+    private Date time;
     @JsonFormat(pattern ="yyyy-mm-dd")
-    private Date created_At;
+    private Date createdAt;
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date cancelledAt;
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date completedAt;
     
 
     public Booking() {
     }
+    public String getBookingId() {
+        return bookingId;
+    }
 
-    public String getCustomer() {
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+    public String getCustomerId() {
         return customer_id;
     }
-    public void setCustomer(String customer_id) {
+    
+    public void setCustomerId(String customer_id) {
         this.customer_id = customer_id;
     }
 
-    public String getStaff() {
-        return staff_id;
+    public String getEmployee() {
+        return employee_id;
     }
-    public void setStaff(String staff_id) {
-        this.staff_id = staff_id;
+    public void setEmployee(String employee_id) {
+        this.employee_id = employee_id;
     } 
 
-    public Date getBookedDate() {
-        return booked_date;
+    public Date getTime() {
+        return time;
     }
-    public void setBookedDate(Date booked_date) {
-        this.booked_date = booked_date;
+    public void setTime(Date time) {
+        this.time = time;
     }
 
     public Date getCreatedDate() {
-        return created_At;
+        return createdAt;
     }
-    public void setCreatedDate(Date created_At) {
-        this.created_At = created_At;
+    public void setCreatedDate(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
 }
