43343a58ec9b4700531539e27ffba6e71950028d
Sam Mellor <s3491308@student.rmit.edu.au>
2020-09-06 14:00:13 +1000

    Add Schedule model
    No need for a Repository or Service until we implement the corresponding REST API methods.
    
    -
    -
    -

---
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/model/Schedule.java
@@ -0,0 +1,41 @@
+package com.rmit.sept.mon15307.backend.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @CreatedDate
+    private Date createdAt;
+
+    @NotNull
+    @LastModifiedDate
+    private Date updatedAt;
+
+    @NotNull
+    private Date date;
+
+    @NotNull
+    private boolean scheduled;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public boolean isScheduled() {
+        return this.scheduled;
+    }
+}
