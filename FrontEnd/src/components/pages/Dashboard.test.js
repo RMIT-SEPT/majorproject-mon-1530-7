c9ea14209175c368692c313013a013fafd3e787e
Sam Mellor <s3491308@student.rmit.edu.au>
2020-09-07 15:24:36 +1000

    Test Dashboard renders without error

---
+++ FrontEnd/src/components/pages/Dashboard.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import Dashboard from './Dashboard';
+import App from '../../App';
+import { mount } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Testing Dashboard route', () => {
+    it('should show Dashboard component for /dashboard router (using memory router)', () => {
+        const component = mount(<MemoryRouter initialEntries={["/dashboard"]}>
+            <App />
+        </MemoryRouter>
+        );
+        expect(component.contains(Dashboard));
+    })
+})
