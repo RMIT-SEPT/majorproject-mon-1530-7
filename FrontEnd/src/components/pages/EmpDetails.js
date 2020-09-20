a366ef7ddf38ef41862aa995e0b9b6af44dbf41f
“Amy <s3783694@student.rmit.edu.au>
2020-09-21 00:18:13 +1000

    Add placeholder links for schedule buttons on Employee Details Page

--- FrontEnd/src/components/pages/EmpDetails.js
+++ FrontEnd/src/components/pages/EmpDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Col, Jumbotron, Row } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 class EmpDetails extends Component {
     render() {
@@ -9,8 +10,8 @@ class EmpDetails extends Component {
                     <Container>
                         <h2 className="h2-main">Employee Details</h2>
                         <Row>
-                            <Col md={{ span: 0, offset: 8 }}><button className="elongated-btn">Add Schedule</button></Col>
-                            <Col md={{ span: 0, offset: 0 }}><button className="elongated-btn">Edit Schedule</button></Col>
+                            <Col md={{ span: 0, offset: 8 }}><Link to="/add-schedule"><button className="elongated-btn">Add Schedule</button></Link></Col>
+                            <Col md={{ span: 0, offset: 0 }}><Link to="/edit-schedule"><button className="elongated-btn">Edit Schedule</button></Link></Col>
                             <Col className="shadow p-4 mb-5 bg-white rounded" id="emp-col" xs={6} md={{ span: 12, offset: 0 }}>
 
                                 <table class="table table-bordered">
