0a57946093de5b328b835ab835cd07fe6b86ea7d
Sam Mellor <s3491308@student.rmit.edu.au>
2020-09-20 23:14:39 +1000

    Fetch available services dynamically when making booking

--- FrontEnd/src/components/layouts/ServiceCard.js
+++ FrontEnd/src/components/layouts/ServiceCard.js
@@ -1,70 +1,84 @@
-import React, {useState} from 'react'
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import Card from 'react-bootstrap/Card';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import React, { Component } from "react";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import Card from "react-bootstrap/Card";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Spinner from "react-bootstrap/Spinner";
+import PropTypes from "prop-types";
 
+class ServiceCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idValue: null,
+    };
 
-function ServiceCard() {
+    this.handleSelection = this.handleSelection.bind(this);
+  }
 
-    const [nameValue, setNameValue] = useState('');
+  handleSelection(value) {
+    this.setState({ idValue: value });
+    this.props.onSelect(value);
+  }
 
-    const products = [
+  renderServices() {
+    if (!this.props.loading && this.props.services.length > 0) {
+      return (
+        <ButtonGroup toggle vertical>
+          {this.props.services.map((service) => (
+            <ToggleButton
+              key={service.id}
+              type="radio"
+              variant="outline-primary"
+              checked={this.state.idValue === service.id}
+              name="radio"
+              value={service.id}
+              size="lg"
+              onChange={(e) => this.handleSelection(e.currentTarget.value)}
+            >
+              {service.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
+      );
+    } else if (!this.props.loading) {
+      // TODO: better empty state
+      return <p>No services found</p>;
+    } else {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    }
+  }
 
-        {
-            id: '1',
-            name: 'Hair-Straightening',
-            description: 'Hair-straightening for ladies using industry-grade equipment.',
-            duration: 3600,
-            price: 4500
-        },
-        {
-            id: '2',
-            name: 'Cut and Blow-Dry',
-            description: 'Customizable cutting service with drying.',
-            duration: 2700,
-            price: 3000
-        },
-        {
-            id: '3',
-            name: 'Perm',
-            description: 'High-quality perm service',
-            duration: 5400,
-            price: 10000
-        }
-
-    ]
-
+  render() {
     return (
-
-        <Card className="shadow p-3 mb-5 bg-white rounded"  border="light" style={{ width: '68rem' }}>
-        
+      <Card
+        className="shadow p-3 mb-5 bg-white rounded"
+        border="light"
+        style={{ width: "68rem" }}
+      >
         <Card.Body>
-
-        <Card.Title>Available Services</Card.Title><br/>
-
-            <ButtonGroup toggle vertical>
-
-                {products.map(service => (
-                    <ToggleButton 
-                        key={service.name}
-                        type="radio" 
-                        variant="outline-primary" 
-                        checked={nameValue === service.name}
-                        name="radio"
-                        value={service.name}
-                        size="lg"
-                        onChange={(e) => setNameValue(e.currentTarget.value)}>
-                        {service.name}
-                    </ToggleButton>
-                ))}
-            
-            </ButtonGroup>
-            
-            </Card.Body>
-
-            </Card>
-
+          <Card.Title>Services</Card.Title>
+          <br />
+          {this.renderServices()}
+        </Card.Body>
+      </Card>
     );
+  }
 }
+
+ServiceCard.propTypes = {
+  services: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      price: PropTypes.number,
+      duration: PropTypes.number,
+    })
+  ),
+};
 
 export default ServiceCard;
--- FrontEnd/src/components/pages/CustomerBookingPage.js
+++ FrontEnd/src/components/pages/CustomerBookingPage.js
@@ -1,79 +1,103 @@
-import React, {Component} from 'react'
-import ServiceCard from '../layouts/ServiceCard';
-import EmployeeCard from '../layouts/EmployeeCard';
-import TimeSelectorCard from '../layouts/TimeSelectorCard';
-import { Container, Jumbotron, CardDeck, Form, Button} from 'react-bootstrap';
+import React, { Component } from "react";
+import ServiceCard from "../layouts/ServiceCard";
+import EmployeeCard from "../layouts/EmployeeCard";
+import TimeSelectorCard from "../layouts/TimeSelectorCard";
+import { Container, Jumbotron, CardDeck, Form, Button } from "react-bootstrap";
 
-class CustomerBookingPageTest extends Component {
+class CustomerBookingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showError: false,
+      showSuccess: false,
+      services: [],
+      loadingServices: true,
+      loadingStaff: true,
+      selectedServiceId: null,
+    };
 
-    constructor(props){
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onServiceSelect = this.onServiceSelect.bind(this);
+  }
 
-        super(props);
-        this.state = {
-            
-            showError:false,
-            showSuccess:false
-        };
+  componentDidMount() {
+    // Only fetch once, on page load
+    // If data is stale booking will be rejected by backend and data will be refreshed
+    // TODO: handle booking conflicts
+    this.fetchServices();
+  }
 
-        this.handleSubmit = this.handleSubmit.bind(this); 
+  onServiceSelect(serviceId) {
+    this.setState({ selectedServiceId: serviceId });
+  }
+
+  fetchServices() {
+    fetch(process.env.REACT_APP_API_URL + "/products")
+      .then((response) => response.json())
+      .then((data) =>
+        // TODO: handle errors
+        this.setState({ services: data["products"], loadingServices: false })
+      );
+  }
+
+  handleSubmit(event) {
+    if (this.state.showError) {
+      alert(
+        "Invalid Booking: Please select a service, employee, and an available time."
+      );
+      event.preventDefault();
+    } else {
+      alert("Booking Successfull!");
+      event.preventDefault();
     }
+  }
 
-    handleSubmit(event){
+  showSuccessAlert = () => {
+    this.setState({ showSuccess: true });
+  };
 
-        if(this.state.showError){
-            alert('Invalid Booking: Please select a service, employee, and an available time.');
-            event.preventDefault();
-        }
-        else{
-            alert('Booking Successfull!');
-            event.preventDefault();
-        }
-        
-    }
+  hideSuccessAlert = () => {
+    this.setState({ showSuccess: false });
+  };
 
-    showSuccessAlert = () =>{
+  showErrorAlert = () => {
+    this.setState({ showError: true });
+  };
 
-        this.setState({showSuccess:true});
-    }
+  hideErrorAlert = () => {
+    this.setState({ showError: false });
+  };
 
-    hideSuccessAlert = () =>{
-
-        this.setState({showSuccess:false})
-    }
-
-    showErrorAlert = () =>{
-
-        this.setState({showError:true});
-    }
-
-    hideErrorAlert = () =>{
-
-        this.setState({showError:false})
-    }
-
-    render() {
-
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <Jumbotron id="jumbotron-cus-book-page">
-                    <h2 className="h2-cus-book-page">Booking</h2>
-                    <Container className="customerBookingPageContainer">
-                        <CardDeck>
-                            <ServiceCard />
-                            <EmployeeCard/>
-                            <TimeSelectorCard/>
-                        </CardDeck>
-                    </Container>
-                    <Container className="makeCusBooking">
-                        <Button variant="primary" size="lg" type="submit"
-                            onClick={this.showErrorAlert}>
-                            Make Booking
-                        </Button>
-                    </Container>
-                </Jumbotron>
-            </Form>
-        )
-    }
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Jumbotron id="jumbotron-cus-book-page">
+          <h2 className="h2-cus-book-page">Booking</h2>
+          <Container className="customerBookingPageContainer">
+            <CardDeck>
+              <ServiceCard
+                services={this.state.services}
+                loading={this.state.loadingServices}
+                onSelect={this.onServiceSelect}
+              />
+              <EmployeeCard />
+              <TimeSelectorCard />
+            </CardDeck>
+          </Container>
+          <Container className="makeCusBooking">
+            <Button
+              variant="primary"
+              size="lg"
+              type="submit"
+              onClick={this.showErrorAlert}
+            >
+              Make Booking
+            </Button>
+          </Container>
+        </Jumbotron>
+      </Form>
+    );
+  }
 }
 
-export default CustomerBookingPageTest;
+export default CustomerBookingPage;
