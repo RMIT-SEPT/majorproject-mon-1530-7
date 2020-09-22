7ec00f43e45276499e0d002e02f6034127cd982e
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-09-22 22:40:34 +1000

    fixed signupurls restricting access to only user pages

--- BackEnd/src/main/java/com/rmit/sept/mon15307/backend/security/SecurityConstant.java
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/security/SecurityConstant.java
@@ -2,7 +2,7 @@ package com.rmit.sept.mon15307.backend.security;
 
 public class SecurityConstant {
 
-    public static final String SIGN_UP_URLS = "/api/**";
+    public static final String SIGN_UP_URLS = "/api/user/**";
     public static final String H2_URL = "/h2-console";
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
