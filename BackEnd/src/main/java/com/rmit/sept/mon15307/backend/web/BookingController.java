5632dd5a958a934a422964fbb970e14971aef95f
Sam Mellor <s3491308@student.rmit.edu.au>
2020-10-05 17:04:11 +1100

    Update GET bookings implementation
    - Perform filtering using ORM rather than in code
    
    Co-authored-by: [Yashwin Sudarshan s3781718] <s3781718@student.rmit.edu.au>

--- BackEnd/src/main/java/com/rmit/sept/mon15307/backend/web/BookingController.java
+++ BackEnd/src/main/java/com/rmit/sept/mon15307/backend/web/BookingController.java
@@ -19,7 +19,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
