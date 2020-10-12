400d0925bc55abe1f2b194f3019754396acfd683
Sam Mellor <s3491308@student.rmit.edu.au>
2020-10-12 23:37:24 +1100

    Fix signup links

--- FrontEnd/src/components/Main.js
+++ FrontEnd/src/components/Main.js
@@ -19,16 +19,20 @@ function Main() {
           </Col>
           <Col id="col-main2">
             <h2 className="h2-main">24/7 Access to online bookings</h2>
-            <p>Your clients will have the ability to book whenever they need,
-            allowing your business to thrive even when you are not looking.
-            At the click of a button, our easy booking system will help you
-            gain more customers at a faster rate. Our API welcomes
-            a variety of businesses and services, and will help reach your
-            business to a larger community and grow your brand.</p>
+            <p>
+              Your clients will have the ability to book whenever they need,
+              allowing your business to thrive even when you are not looking. At
+              the click of a button, our easy booking system will help you gain
+              more customers at a faster rate. Our API welcomes a variety of
+              businesses and services, and will help reach your business to a
+              larger community and grow your brand.
+            </p>
           </Col>
         </Row>
         <Row id="row-center-main">
-          <Col xs={6} md={{ offset: 0 }}><h2 className="h2-center">Our Features</h2></Col>
+          <Col xs={6} md={{ offset: 0 }}>
+            <h2 className="h2-center">Our Features</h2>
+          </Col>
         </Row>
         <Row id="row-center">
           <Col xs={6} md={{ offset: 0 }}>
@@ -42,23 +46,48 @@ function Main() {
           </Col>
         </Row>
         <Row id="row-center">
-          <Col xs={6} md={{ offset: 0 }}><h4 className="h4-main2">Ease-of-use</h4></Col>
-          <Col xs={6} md={{ offset: 1 }}><h4 className="h4-main2">Rapid Growth</h4></Col>
-          <Col xs={6} md={{ offset: 1 }}><h4 className="h4-main2">Track Bookings</h4></Col>
+          <Col xs={6} md={{ offset: 0 }}>
+            <h4 className="h4-main2">Ease-of-use</h4>
+          </Col>
+          <Col xs={6} md={{ offset: 1 }}>
+            <h4 className="h4-main2">Rapid Growth</h4>
+          </Col>
+          <Col xs={6} md={{ offset: 1 }}>
+            <h4 className="h4-main2">Track Bookings</h4>
+          </Col>
         </Row>
         <Row id="row-center">
-          <Col xs={6} md={{ offset: 0 }}><p>Our simple and easy-to-use API is great for beginners and starters. Design is intuitive and will improve productivity.</p></Col>
-          <Col xs={6} md={{ offset: 1 }}><p>Our API will help grow and further advance your business in the long run. Produce noticeable results with us.</p></Col>
-          <Col xs={6} md={{ offset: 1 }}><p>With our user-friendly API, you will be able to quickly and easily track current and upcoming bookings.</p></Col>
+          <Col xs={6} md={{ offset: 0 }}>
+            <p>
+              Our simple and easy-to-use API is great for beginners and
+              starters. Design is intuitive and will improve productivity.
+            </p>
+          </Col>
+          <Col xs={6} md={{ offset: 1 }}>
+            <p>
+              Our API will help grow and further advance your business in the
+              long run. Produce noticeable results with us.
+            </p>
+          </Col>
+          <Col xs={6} md={{ offset: 1 }}>
+            <p>
+              With our user-friendly API, you will be able to quickly and easily
+              track current and upcoming bookings.
+            </p>
+          </Col>
         </Row>
         <Row id="row-main">
           <Col xs={6} md={{ span: 6, offset: 0 }} id="col-main3">
             <h2 className="h2-main">Get Started With Us Today!</h2>
-            <p>Join us today and kickstart your business ASAP. We guarantee rapid business growth
-              and improved management with our API. We offer many other features available for your
-              business, so get started to find out more!
+            <p>
+              Join us today and kickstart your business ASAP. We guarantee rapid
+              business growth and improved management with our API. We offer
+              many other features available for your business, so get started to
+              find out more!
             </p>
-            <Link href="/signup"><button className="btn-large">Get Started</button></Link>
+            <Link to="/signup">
+              <button className="btn-large">Get Started</button>
+            </Link>
           </Col>
           <Col xs={6} md={{ span: 4, offset: 0 }}>
             <img src={Image3} alt="main_img" height="600"></img>
@@ -66,7 +95,7 @@ function Main() {
         </Row>
       </Container>
     </Jumbotron>
-  )
+  );
 }
 
 export default Main;
